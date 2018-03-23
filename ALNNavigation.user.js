7281960f099e22ab667e3dad7fd6853738d68410
acwilliams <acwilliams@liquidweb.com>
2018-03-23 16:53:34 -0400

    Version 0.0.1

--- ALNNavigation.user.js
+++ ALNNavigation.user.js
@@ -10,8 +10,32 @@
 // @grant			GM_deleteValue
 // @require     	https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
 // @include			http://www.all-night-laundry.com/post/*
-// @updateURL		
-// @downloadURL		
+// @updateURL		https://github.com/sporks5000/ALNNavigation/raw/master/ALNNavigation.meta.js
+// @downloadURL		https://github.com/sporks5000/ALNNavigation/raw/master/ALNNavigation.user.js
 // ==/UserScript==
 
+var alnnDebugging = true;
+
+function debug (msg) { if (alnnDebugging) { console.log('[' + GM_info.script.name + ' - ' + new Date().toLocaleString() + '] ' + msg); } }
+
+function init() {
+	debug('page loaded');
+	
+	var navBar = $('#navigation');
+	var content = $('#post');
+	debug('Adding navigation to the top');
+	var navBar2 = navBar.clone(true).attr('id','navigation').prependTo(post);
+	debug('Adding navigation to the bottom');
+	var navBar3 = navBar.clone(true).attr('id','navigation').appendTo(post);
+	navBar2.after('<br />');
+	navBar3.before('<br />');
+	var notes = $('#notes');
+	if ( notes ) {
+		debug('Adding navigation after notes');
+		var navBar4 = navBar.clone(true).attr('id','navigation').appendTo(notes);
+		navBar4.before('<br />');
+	}
+}
+
+init();
 
