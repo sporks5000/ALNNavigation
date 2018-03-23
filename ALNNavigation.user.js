82bac35856c804e57e23ea71ba6860b74d819984
acwilliams <acwilliams@liquidweb.com>
2018-03-23 16:18:03 -0400

    initial

---
+++ ALNNavigation.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name			ALNNavigation
+// @description		Additional Navagation buttons for All Night Laundry
+// @version			0.0.1
+// @author			sporks5000@gmail.com
+// @grant        	GM_xmlhttpRequest
+// @grant			GM_addStyle
+// @grant        	GM_getValue
+// @grant        	GM_setValue
+// @grant			GM_deleteValue
+// @require     	https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+// @include			http://www.all-night-laundry.com/post/*
+// @updateURL		
+// @downloadURL		
+// ==/UserScript==
+
+
